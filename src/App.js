7890949455a831003c97bd3fc02840e928201413
endlessiris <endlessiris@naver.com>
2020-07-15 16:32:44 +0900

    customer add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/128/128/1',
+    'name' :  '니엘',
+    'birthday' : '830407',
+    'gender' : '남자',
+    'job' : 'programmer'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/128/128/2',
+    'name' :  '다니',
+    'birthday' : '930407',
+    'gender' : '여자',
+    'job' : '직장인'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/128/128/3',
+    'name' :  '니엘',
+    'birthday' : '950407',
+    'gender' : '여자',
+    'job' : '디자이너'
+  },
+]
+  
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+           return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+           );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
