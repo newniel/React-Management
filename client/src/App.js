4d79040634db1c7143ac72229b2ddf0ee5a77e03
endlessiris <endlessiris@naver.com>
2020-07-17 17:08:25 +0900

    add progressbar

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import TableBody from '@material-ui/core/TableBody'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import { withStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
 
 const styles = theme => ({
   root: {
@@ -17,6 +19,9 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
 
@@ -51,10 +56,12 @@ const styles = theme => ({
 
 class App extends Component {
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -64,6 +71,12 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    console.log('completed', completed);
+    this.setState({ completed : completed >= 100 ? 0 : completed + 1});
   }
 
   render() {
@@ -93,7 +106,13 @@ class App extends Component {
                   job={c.job}
                 />
               );
-              }): ""}
+              }) : 
+                <TableRow>
+                  <TableCell colspan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                  </TableCell>
+                </TableRow>
+              }
           </TableBody>
         </Table>
       </Paper>
