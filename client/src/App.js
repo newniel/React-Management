a255ba2f94c7a7598324a9e7dd5580351ee7c1f0
endlessiris <endlessiris@naver.com>
2020-07-17 16:35:11 +0900

    proxy add

--- client/src/App.js
+++ client/src/App.js
@@ -20,36 +20,52 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    'id' : 1,
-    'image' : 'https://placeimg.com/128/128/1',
-    'name' :  '니엘',
-    'birthday' : '830407',
-    'gender' : '남자',
-    'job' : 'programmer'
-  },
-  {
-    'id' : 2,
-    'image' : 'https://placeimg.com/128/128/2',
-    'name' :  '다니',
-    'birthday' : '930407',
-    'gender' : '여자',
-    'job' : '직장인'
-  },
-  {
-    'id' : 3,
-    'image' : 'https://placeimg.com/128/128/3',
-    'name' :  '니엘',
-    'birthday' : '950407',
-    'gender' : '여자',
-    'job' : '디자이너'
-  },
-]
+// const customers = [
+//   {
+//     'id' : 1,
+//     'image' : 'https://placeimg.com/128/128/1',
+//     'name' :  '니엘',
+//     'birthday' : '830407',
+//     'gender' : '남자',
+//     'job' : 'programmer'
+//   },
+//   {
+//     'id' : 2,
+//     'image' : 'https://placeimg.com/128/128/2',
+//     'name' :  '다니',
+//     'birthday' : '930407',
+//     'gender' : '여자',
+//     'job' : '직장인'
+//   },
+//   {
+//     'id' : 3,
+//     'image' : 'https://placeimg.com/128/128/3',
+//     'name' :  '니엘',
+//     'birthday' : '950407',
+//     'gender' : '여자',
+//     'job' : '디자이너'
+//   },
+// ]
   
 
 
 class App extends Component {
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi =  async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props
     return (
@@ -66,9 +82,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {
-              customers.map(c => {
-              return (
+            {this.state.customers ? this.state.customers.map(c => {return (
                 <Customer
                   key={c.id}
                   id={c.id}
@@ -79,8 +93,7 @@ class App extends Component {
                   job={c.job}
                 />
               );
-              })
-            }
+              }): ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            "id": 1,
+            "image": "https://placeimg.com/128/128/1",
+            "name": "니엘",
+            "birthday": "830407",
+            "gender": "남자",
+            "job": "programmer"
+          },
+          {
+            "id": 2,
+            "image": "https://placeimg.com/128/128/2",
+            "name": "디니",
+            "birthday": "930407",
+            "gender": "여자",
+            "job": "디자이너"
+          },
+          {
+            "id": 3,
+            "image": "https://placeimg.com/128/128/3",
+            "name": "엘",
+            "birthday": "950407",
+            "gender": "여자",
+            "job": "기획자"
+          }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
